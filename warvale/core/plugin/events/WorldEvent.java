005046ed6f784527bf568a673d635125dce21b62
Draem <owen@gimbel.ca>
2017-05-08 17:23:54 -0400

    fixed some things and actually used bukkit's sync delayed task .c.

--- warvale/core/plugin/events/WorldEvent.java
+++ warvale/core/plugin/events/WorldEvent.java
@@ -1,10 +1,13 @@
 package warvale.core.plugin.events;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.player.PlayerPickupItemEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.util.Vector;
 import warvale.core.plugin.Main;
 import warvale.core.plugin.utils.NumberUtils;
@@ -16,33 +19,41 @@ import java.util.concurrent.TimeUnit;
  */
 public class WorldEvent implements Listener {
 
+    private Main main;
+
     public WorldEvent(Main plugin) {
         plugin.getServer().getPluginManager().registerEvents(this, plugin);
+        main = plugin;
     }
 
     @EventHandler
     public void onBlockBreak(BlockBreakEvent event) {
         if (event.getBlock().getType().equals(Material.IRON_ORE)) {
             event.getPlayer().giveExp(10);
-            for (int i = 1; i < NumberUtils.random(3, 1); i++) {
+            for (int i = 0; i < NumberUtils.random(3, 1) + 1; i++) {
                 event.getPlayer().getInventory().addItem(new ItemStack(Material.IRON_INGOT));
             }
             event.setCancelled(true);
             event.getBlock().breakNaturally(new ItemStack(Material.AIR));
             event.getBlock().setType(Material.STONE);
-            new Thread(() -> {
+            Bukkit.getScheduler().scheduleSyncDelayedTask(main, () -> {
                 try {
-                    for (int i = 1; i < NumberUtils.random(5, 2); i++) {
+                    for (int i = 0; i < NumberUtils.random(5, 2) + 1; i++) {
                         event.getPlayer().getWorld().dropItemNaturally(event.getBlock().getLocation().setDirection(Vector.getRandom()), new ItemStack(Material.IRON_NUGGET));
                     }
-
-                    TimeUnit.SECONDS.sleep(10);
+                    TimeUnit.MILLISECONDS.sleep(10000);
                     event.getBlock().setType(Material.IRON_ORE);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-            }).run();
+            }, 1);
         }
     }
 
+    @EventHandler
+    public void onItemPickup(PlayerPickupItemEvent event) {
+        if (event.getItem().getItemStack().getType().equals(Material.IRON_NUGGET)) {
+            event.setCancelled(true);
+        }
+    }
 }
