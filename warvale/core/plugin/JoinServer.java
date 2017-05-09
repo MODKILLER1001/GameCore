569dc50a4d1e51757abb0595614a8a9c4c3371a5
Pixelific <pixelificz@gmail.com>
2017-05-08 22:47:12 -0400

    add a team selector item for spectators (part of future gui)

--- warvale/core/plugin/JoinServer.java
+++ warvale/core/plugin/JoinServer.java
@@ -1,8 +1,14 @@
 package warvale.core.plugin;
 
+import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
@@ -13,8 +19,9 @@ public class JoinServer implements Listener {
     public JoinServer(Main plugin) {
         plugin.getServer().getPluginManager().registerEvents(this, plugin);
     }
-
+  
     @EventHandler
+
     public void onPlayerJoinEvent(PlayerJoinEvent event) {
         event.getPlayer().getInventory().clear();
         event.getPlayer().sendMessage(ChatColor.GRAY + "Welcome back to " + ChatColor.DARK_RED + "Warvale!");
@@ -23,7 +30,33 @@ public class JoinServer implements Listener {
         
         if (Main.getSpectatorTeam().getEntries().contains(event.getPlayer().getName())) {
         	event.getPlayer().setAllowFlight(true);
+            ItemStack teamselect = new ItemStack(Material.ENCHANTED_BOOK, 1); {
+            ItemMeta spawnmeta = teamselect.getItemMeta();
+            
+            spawnmeta.setDisplayName(ChatColor.RESET + "" + ChatColor.AQUA + "Team Selector");
+            teamselect.setItemMeta(spawnmeta);
+            
+            event.getPlayer().getInventory().setItem(4, teamselect);
+            }
         }
     }
-}
+    @EventHandler(priority = EventPriority.LOWEST) 
+    public void onMove(InventoryClickEvent e){
+        ItemStack i = e.getWhoClicked().getInventory().getItem(4);
+        if(i != null)
+        {
+            if(e.getSlot() == 4 && i.getType() == Material.ENCHANTED_BOOK) // Get if the clicked slot was the 4th
+            {
+                e.setCancelled(true);
+            }
+        }
+    }
+    
+    @EventHandler
+    public void onPlayerDropItem(PlayerDropItemEvent event) {
 
+    if (event.getItemDrop().getItemStack().getType() == Material.ENCHANTED_BOOK) {
+    event.setCancelled(true);
+    }
+    }
+     }
