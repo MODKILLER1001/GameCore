6f76f8ba9d3ee16f0132260c1f107fc34e2050d4
Pixelific <pixelificz@gmail.com>
2017-05-14 23:00:05 -0400

    fix version

--- warvale/core/plugin/commands/Version.java
+++ warvale/core/plugin/commands/Version.java
@@ -9,6 +9,7 @@ public class Version implements CommandExecutor {
 
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (command.getName().equalsIgnoreCase("gamever")) {
+            sender.sendMessage(ChatColor.GRAY + "Gamecore is running on version 1.14");
             return true;
         }
         return false;
