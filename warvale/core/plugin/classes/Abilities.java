f3048e6fe03d88bd09da60ec3e26a10e6c92817a
Draem <owen@gimbel.ca>
2017-05-20 16:21:37 -0400

    ability thing

--- warvale/core/plugin/classes/Abilities.java
+++ warvale/core/plugin/classes/Abilities.java
@@ -1,22 +1,14 @@
 package warvale.core.plugin.classes;
 
-import org.bukkit.Material;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
-
-import warvale.core.plugin.Main;
 
 public class Abilities {
 
     public static void detectClass(Player player) {
-
         if (ClassManager.getClassForPlayer(player.getName()) != null) {
-            ClassManager.getClassForPlayer(player.getName()).getName();
-
-            if (ClassManager.getClassForPlayer(player.getName()).getName().equalsIgnoreCase("Soldier")) {
-                ItemStack soldier_icon = new ItemStack(Material.FIREWORK_CHARGE);
-                System.out.println("Soldier");
-            }
+           Class clazz = ClassManager.getClassForPlayer(player.getName());
+           assert clazz != null;
+           System.out.println(player.getName() + "'s Class: " + clazz.getName());
         }
     }
 
