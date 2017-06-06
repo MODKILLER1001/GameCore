2933cccbcdd790614c1e43d7a7a8b7ca8654b5a1
Eric <ehock7229@outlook.com>
2017-06-05 21:13:29 -0400

    fixes

--- src/main/java/net/warvale/core/Main.java
+++ src/main/java/net/warvale/core/Main.java
@@ -456,8 +456,8 @@ public class Main extends JavaPlugin implements Listener {
 					"CREATE TABLE `maps` (" +
 							"`id` BIGINT NOT NULL AUTO_INCREMENT, " +
 							"`map` VARCHAR(255) NOT NULL," +
-							"`value` TEXT NOT NULL," +
 							"`key` TEXT NOT NULL," +
+							"`value` TEXT NOT NULL," +
 							"PRIMARY KEY (`id`)," +
 							"INDEX `map_key` (`map`)" +
 							");");
