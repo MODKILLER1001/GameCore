1c7a3ffb56ad0e93cdf2c5888eb3a5f388a2d1a7
AAces <djp0908@yahoo.com>
2017-06-20 10:49:27 -0600

    Updated region protect to disallow placement too

--- src/main/java/net/warvale/core/game/logic/RegionProtection.java
+++ src/main/java/net/warvale/core/game/logic/RegionProtection.java
@@ -6,6 +6,7 @@ import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
 
 /**
  * Created by Draem on 6/19/2017.
@@ -14,6 +15,15 @@ public class RegionProtection implements Listener {
 
     @EventHandler
     public void onBlockBreak(BlockBreakEvent event) {
+        for (GameMap map : GameMap.getMaps()) {
+            if (new ConquestMap(map.getName()).crossCheckCoords(event.getBlock().getX(), event.getBlock().getY(), event.getBlock().getZ())) {
+                event.getPlayer().sendMessage(ChatColor.RED + "You may not edit the blocks here.");
+                event.setCancelled(true);
+            }
+        }
+    }
+    @EventHandler
+    public void onBlockPlace(BlockPlaceEvent event) {
         for (GameMap map : GameMap.getMaps()) {
             if (new ConquestMap(map.getName()).crossCheckCoords(event.getBlock().getX(), event.getBlock().getY(), event.getBlock().getZ())) {
                 event.getPlayer().sendMessage(ChatColor.RED + "You may not edit the blocks here.");
