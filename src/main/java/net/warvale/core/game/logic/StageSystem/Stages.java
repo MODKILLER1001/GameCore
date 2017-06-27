1edc8466a7bac10cf2b9ea9e22484e61db56f397
RONTheCookie <ronthecookie0101@gmail.com>
2017-06-27 19:15:45 +0300

    fancy messages when new stages arrive.

--- src/main/java/net/warvale/core/game/logic/StageSystem/Stages.java
+++ src/main/java/net/warvale/core/game/logic/StageSystem/Stages.java
@@ -4,6 +4,8 @@ import net.warvale.core.Main;
 import net.warvale.core.game.CoreBlock;
 import net.warvale.core.game.CoreState;
 import net.warvale.core.game.end.GameEnd;
+import net.warvale.core.message.MessageManager;
+import net.warvale.core.message.PrefixType;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
@@ -15,7 +17,10 @@ public class Stages {
     private Stage CurrentStage;
     public Stage getCurrentStage() {return CurrentStage;}
     private Plugin pl = Main.get();
-
+    private void quickStageAnnounce() {
+        String stagestring = CurrentStage.toString().toLowerCase();
+        MessageManager.broadcast(PrefixType.MAIN, "Stage is now: " + stagestring.substring(0, 1).toUpperCase() + stagestring.substring(1) + ".");
+    }
     public void initStages() {
         CurrentStage = Stage.INVINCIBLE_CORE;
         // game_start
@@ -27,6 +32,7 @@ public class Stages {
             @Override
             public void run() {
            new Bosses().initBoss();
+           quickStageAnnounce();
            new CoreBlock().setCoreState(CoreState.BREAKABLE);
            CurrentStage = Stage.BOSS_DIAMOND;
             }
@@ -39,6 +45,7 @@ public class Stages {
             @Override
             public void run() {
             CurrentStage = Stage.CORE_BREAK_FAST;
+                quickStageAnnounce();
             new CoreBlock().setCoreState(CoreState.SPEED_BREAK);
             }
 
@@ -50,6 +57,7 @@ public class Stages {
             @Override
             public void run() {
             CurrentStage = Stage.DEATHMATCH;
+                quickStageAnnounce();
             new CoreBlock().setCoreState(CoreState.INSTANT_BREAK);
             }
 
